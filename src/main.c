89f0bd24abe3d824dbca4d025b5f1061a166fc85
Karandeep Singh <kasingh@bess-f1r5s9.clusters.42paris.fr>
2024-04-06 13:42:13 +0200

    start minishellt

---
+++ src/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kasingh <kasingh@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/30 11:53:33 by kasingh           #+#    #+#             */
+/*   Updated: 2024/04/05 14:58:09 by kasingh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	main(int ac, char **av, char **env)
+{
+	(void)av;
+	if (ac != 1)
+	{
+		printf("Error: Too many arguments\n");
+		return (1);
+	}
+	printf("Hello, World!\n");
+	return (0);
+}
