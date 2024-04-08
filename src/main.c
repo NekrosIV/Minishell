e4653d4c3ac6b0718e5aeb2771841450611f98d8
Pierre-Loup Scala <pscala@made-f0Ar3s9.clusters.42paris.fr>
2024-04-08 13:58:24 +0200

    jojolion

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pscala <pscala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 11:53:33 by kasingh           #+#    #+#             */
-/*   Updated: 2024/04/08 13:33:07 by pscala           ###   ########.fr       */
+/*   Updated: 2024/04/08 13:50:37 by pscala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	main(int ac, char **av, char **env)
 {
+	char	*line;
+
 	(void)av;
 	if (ac != 1)
 	{
@@ -22,9 +24,9 @@ int	main(int ac, char **av, char **env)
 	}
 	printf("Hello, World!\n");
 	return (0);
-}
-
-else()
-{
-	poqwerqwerqwerqwer
+	while (1)
+	{
+		line = readline("minirt>");
+		printf("line = %s", line);
+	}
 }
