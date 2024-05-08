24432c2c3eab599fdcfe1d45b2cb744327efce2a
kasingh <kasingh@student.42.fr>
2024-05-08 22:31:57 +0200

    je continue sur le check syntax des ()

--- src/parsing/check_syntax_bonus.c
+++ src/parsing/check_syntax_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kasingh <kasingh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 13:10:54 by kasingh           #+#    #+#             */
-/*   Updated: 2024/05/07 17:59:01 by kasingh          ###   ########.fr       */
+/*   Updated: 2024/05/08 18:35:36 by kasingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,73 +90,161 @@ int	check_parenth_close(t_word *tmp, t_var *var)
 	return (0);
 }
 
-int	is_good_arg_in_parenth(t_word *lexer, t_var *var)
+// int	is_good_arg_in_parenth(t_word *lexer, t_var *var)
+// {
+// 	int		count_or_and;
+// 	int		count_parenth;
+// 	char	*err;
+// 	bool	pipe;
+// 	int		count_cmd;
+// 	bool	can_i;
+// 	t_word	*tmp;
+
+// 	tmp = 0;
+// 	err = NULL;
+// 	can_i = true;
+// 	while (lexer)
+// 	{
+// 		if (lexer->token == PARENTH_OPEN)
+// 		{
+// 			count_or_and = 0;
+// 			count_parenth = 0;
+// 			count_cmd = 0;
+// 			pipe = false;
+// 			tmp = lexer->next;
+// 			while (tmp)
+// 			{
+// 				if (tmp->token == OR || tmp->token == AND)
+// 					count_or_and++;
+// 				if (is_cmd(tmp->token))
+// 					count_cmd++;
+// 				if (tmp->token == PIPE)
+// 					pipe = true;
+// 				if (tmp->token == PARENTH_OPEN)
+// 				{
+// 					count_parenth = 2;
+// 					while (count_parenth > 1)
+// 					{
+// 						tmp = tmp->next;
+// 						if (!err && tmp->token != SPACES)
+// 							err = tmp->word;
+// 						if (tmp->token == PIPE)
+// 							pipe = true;
+// 						if (tmp->token == PARENTH_OPEN)
+// 							count_parenth++;
+// 						if (tmp->token == PARENTH_CLOSE)
+// 							count_parenth--;
+// 						if (count_parenth == 1)
+// 							tmp = tmp->next;
+// 					}
+// 				}
+// 				if (count_parenth == 1 && can_i == true && tmp->token != SPACES)
+// 					(err = tmp->word, can_i = false, printf("err: %s\n", err));
+// 				if (tmp->token == PARENTH_CLOSE)
+// 					break ;
+// 				tmp = tmp->next;
+// 			}
+// 			if (count_parenth == 0 && count_cmd != 0)
+// 				;
+// 			else if (pipe == true && count_parenth != 0)
+// 				return (free_error(var, E_SYNTAX, "|", -2), -1);
+// 			else if (err != NULL && count_or_and <= 0)
+// 				return (free_error(var, E_SYNTAX, err, -2), -1);
+// 			else if (count_or_and <= 0)
+// 				return (free_error(var, E_SYNTAX, "(", -2), -1);
+// 			else if (count_cmd <= 0)
+// 				return (free_error(var, E_SYNTAX, "(", -2), -1);
+// 		}
+// 		lexer = lexer->next;
+// 	}
+// 	return (0);
+// }
+
+t_word	*end_of_parenth(t_word *lexer)
 {
-	int		count_or_and;
-	int		count_parenth;
-	char	*err;
-	bool	pipe;
-	int		count_cmd;
-	bool	can_i;
+	int		count;
 	t_word	*tmp;
 
-	tmp = 0;
-	err = NULL;
-	can_i = true;
-	while (lexer)
+	count = 1;
+	tmp = lexer->next;
+	while (tmp)
 	{
-		if (lexer->token == PARENTH_OPEN)
-		{
-			count_or_and = 0;
-			count_parenth = 0;
-			count_cmd = 0;
-			pipe = false;
-			tmp = lexer->next;
-			while (tmp)
-			{
-				if (tmp->token == OR || tmp->token == AND)
-					count_or_and++;
-				if (is_cmd(tmp->token))
-					count_cmd++;
-				if (tmp->token == PIPE)
-					pipe = true;
-				if (tmp->token == PARENTH_OPEN)
-				{
-					count_parenth = 2;
-					while (count_parenth > 1)
-					{
-						tmp = tmp->next;
-						if (!err && tmp->token != SPACES)
-							err = tmp->word;
-						if (tmp->token == PIPE)
-							pipe = true;
-						if (tmp->token == PARENTH_OPEN)
-							count_parenth++;
-						if (tmp->token == PARENTH_CLOSE)
-							count_parenth--;
-						if (count_parenth == 1)
-							tmp = tmp->next;
-					}
-				}
-				if (count_parenth == 1 && can_i == true && tmp->token != SPACES)
-					(err = tmp->word, can_i = false, printf("err: %s\n", err));
-				if (tmp->token == PARENTH_CLOSE)
-					break ;
-				tmp = tmp->next;
-			}
-			if (count_parenth == 0 && count_cmd != 0)
-				;
-			else if (pipe == true && count_parenth != 0)
-				return (free_error(var, E_SYNTAX, "|", -2), -1);
-			else if (err != NULL && count_or_and <= 0)
-				return (free_error(var, E_SYNTAX, err, -2), -1);
-			else if (count_or_and <= 0)
-				return (free_error(var, E_SYNTAX, "(", -2), -1);
-			else if (count_cmd <= 0)
-				return (free_error(var, E_SYNTAX, "(", -2), -1);
-		}
-		lexer = lexer->next;
+		if (tmp->token == PARENTH_OPEN)
+			count++;
+		if (tmp->token == PARENTH_CLOSE)
+			count--;
+		if (count == 0)
+			break ;
+		tmp = tmp->next;
 	}
+	return (tmp);
+}
+
+int	check_out_pareth(t_word *lexer, t_var *var)
+{
+	t_word	*tmp;
+	bool	parenthesis;
+
+	tmp = lexer->prev;
+	parenthesis = false;
+	while (tmp)
+	{
+		if (tmp->token == SPACES)
+			tmp = tmp->prev;
+		else
+			break ;
+	}
+	if (tmp != NULL && tmp->token != PIPE && tmp->token != OR
+		&& tmp->token != AND && tmp->token != PARENTH_OPEN)
+		return (-1);
+	tmp = end_of_parenth(lexer);
+	tmp = tmp->next;
+	while (tmp)
+	{
+		if (tmp->token == SPACES || tmp->token == END)
+			tmp = tmp->next;
+		else
+			break ;
+	}
+	if (tmp != NULL && tmp->token != PIPE && tmp->token != OR
+		&& tmp->token != AND && tmp->token != PARENTH_CLOSE)
+		return (-1);
+	return (0);
+}
+
+int	check_in_parenth(t_word *lexer, t_var *var)
+{
+	t_word	*tmp;
+	bool	parenthesis;
+
+	tmp = lexer->next;
+	parenthesis = false;
+	while (tmp)
+	{
+		if (tmp->token == SPACES || is_cmd(tmp->token))
+			tmp = tmp->next;
+		else
+			break ;
+	}
+	if (tmp->token == PARENTH_OPEN)
+		parenthesis = true;
+	else if (tmp != NULL && tmp->token != OR && tmp->token != AND
+		&& tmp->token != PARENTH_OPEN)
+		return (-1);
+	tmp = end_of_parenth(lexer);
+	tmp = tmp->prev;
+	while (tmp)
+	{
+		if (tmp->token == SPACES || is_cmd(tmp->token))
+			tmp = tmp->prev;
+		else
+			break ;
+	}
+	if (tmp->token == PARENTH_CLOSE && parenthesis == true)
+		return (-1);
+	else if (tmp != NULL && tmp->token != OR && tmp->token != AND
+		&& tmp->token != PARENTH_CLOSE)
+		return (-1);
 	return (0);
 }
 
@@ -164,7 +252,17 @@ int	check_syntax_parenth(t_word *lexer, t_var *var)
 {
 	if (check_parenth_close(lexer, var) == -1)
 		return (-1);
-	if (is_good_arg_in_parenth(lexer, var) == -1)
-		return (-1);
+	// if (is_good_arg_in_parenth(lexer, var) == -1)
+	// 	return (-1);
+	while (lexer)
+	{
+		if (lexer->token == PARENTH_OPEN)
+		{
+			if (check_out_pareth(lexer, var) == -1 || check_in_parenth(lexer,
+					var) == -1)
+				return (free_error(var, E_SYNTAX, "()", -2), -1);
+		}
+		lexer = lexer->next;
+	}
 	return (0);
 }
